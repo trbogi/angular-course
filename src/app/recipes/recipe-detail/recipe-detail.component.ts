cb4e8365a94d394e84467daa570825ee7e5fef2a
trbogi <trencsenyi.bogi@gmail.com>
2022-10-24 15:21:49 +0200

    Show recipe details

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
+import {Input} from "@angular/core";
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
+  @Input() selectedRecipe: Recipe
 
   constructor() { }
 
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model'
 
 @Component({
@@ -7,12 +7,20 @@ import {Recipe} from '../recipe.model'
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>()
+
   recipes: Recipe[] = [
-    new Recipe("Test Recipe", "Test Description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg")
+    new Recipe("Test Recipe", "Test Description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg"),
+    new Recipe("Test Recipe 2", "Test Description 2", "https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&resize=504%2C458")
+
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelectRecipe(recipe: Recipe): void {
+    this.recipeWasSelected.emit(recipe)
   }
 
 }
