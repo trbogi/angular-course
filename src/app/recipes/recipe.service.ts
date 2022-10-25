6865e4969837e71ca0e2f320ae22cce0e23c021c
trbogi <trencsenyi.bogi@gmail.com>
2022-10-25 12:29:21 +0200

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,21 @@
 import {Recipe} from "./recipe.model";
 import {EventEmitter} from "@angular/core";
+import {Ingredient} from "../shared/ingredient.model";
 
 export class RecipeService{
   selectedRecipe = new EventEmitter<Recipe>()
 
   private recipes: Recipe[] = [
-      new Recipe("Test Recipe", "Test Description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg"),
-      new Recipe("Test Recipe 2", "Test Description 2", "https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&resize=504%2C458")
+      new Recipe("Test Recipe",
+        "Test Description",
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg",
+        [new Ingredient('meat', 2), new Ingredient('tomato', 3),
+          new Ingredient('potato', 10),]),
+      new Recipe("Test Recipe 2",
+        "Test Description 2",
+        "https://images.immediate.co.uk/production/volatile/sites/30/2022/05/Airfryer-hero-image-2a442a6.jpg?quality=90&resize=504%2C458",
+        [new Ingredient('meat', 2), new Ingredient('avocado', 3),
+          new Ingredient('lettuce', 3),])
   ]
 
   get(): Recipe[]{
