bb74c13e14747a110a379c2c09697ff8831767a1
trbogi <trencsenyi.bogi@gmail.com>
2022-10-26 15:23:12 +0200

    Use Subject instead of EventEmitter in shopping-list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import {Ingredient} from "../shared/ingredient.model";
-import {EventEmitter} from "@angular/core";
+import {Subject} from "rxjs";
 
 export class ShoppingListService{
-  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  ingredientsChanged = new Subject<Ingredient[]>()
 
   private ingredients: Ingredient[] = [
     new Ingredient("apple", 2),
@@ -15,11 +15,11 @@ export class ShoppingListService{
 
   addIngredient(ingredient: Ingredient){
     this.ingredients.push(ingredient)
-    this.ingredientsChanged.emit(this.ingredients)
+    this.ingredientsChanged.next(this.ingredients)
   }
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients)
-    this.ingredientsChanged.emit(this.ingredients)
+    this.ingredientsChanged.next(this.ingredients)
   }
 }
