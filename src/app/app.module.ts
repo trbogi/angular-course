79c213dd9190d8f2a4b5cca2af75d36df9c5c25c
trbogi <trencsenyi.bogi@gmail.com>
2022-10-27 16:41:53 +0200

    Console log recipe form content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HeaderComponent} from "./header/header.component";
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
@@ -33,6 +33,7 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
     imports: [
       BrowserModule,
       FormsModule,
+      ReactiveFormsModule,
       AppRoutingModule
     ],
   providers: [ShoppingListService],
--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'app-recipe-edit',
@@ -9,14 +11,38 @@ import {ActivatedRoute, Params} from "@angular/router";
 export class RecipeEditComponent implements OnInit {
   editMode: boolean = false
   id: number
+  recipeForm: FormGroup
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params:Params) => {
       this.id = +params['id']
       this.editMode = params['id'] != null
+      this.initForm()
     })
+  }
+
+  private initForm(): void {
+    let recipeName = ''
+    let imgPath = ''
+    let description = ''
+
+    if (this.editMode){
+      let recipe = this.recipeService.getRecipeById(this.id)
+      recipeName = recipe.name
+      imgPath = recipe.imagePath
+      description = recipe.description
+    }
+    this.recipeForm = new FormGroup<any>({
+      name: new FormControl(recipeName),
+      imgPath: new FormControl(imgPath),
+      description: new FormControl(description),
+    })
+  }
+
+  onSubmit(){
+    console.log(this.recipeForm)
   }
 
 }
