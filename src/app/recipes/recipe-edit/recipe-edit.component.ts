2858e617b4b389bb31d649f012a010e0a6ba2d07
trbogi <trencsenyi.bogi@gmail.com>
2022-10-28 13:22:18 +0200

    Fix git bug

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Params} from "@angular/router";
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormControl, FormGroup, Validators} from "@angular/forms";
 import {RecipeService} from "../recipe.service";
 
 @Component({
@@ -16,33 +16,7 @@ export class RecipeEditComponent implements OnInit {
   constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe((params:Params) => {
-      this.id = +params['id']
-      this.editMode = params['id'] != null
-      this.initForm()
-    })
   }
 
-  private initForm(): void {
-    let recipeName = ''
-    let imgPath = ''
-    let description = ''
-
-    if (this.editMode){
-      let recipe = this.recipeService.getRecipeById(this.id)
-      recipeName = recipe.name
-      imgPath = recipe.imagePath
-      description = recipe.description
-    }
-    this.recipeForm = new FormGroup<any>({
-      name: new FormControl(recipeName),
-      imgPath: new FormControl(imgPath),
-      description: new FormControl(description),
-    })
-  }
-
-  onSubmit(){
-    console.log(this.recipeForm)
-  }
 
 }
