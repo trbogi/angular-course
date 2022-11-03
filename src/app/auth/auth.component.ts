78005c2752ba6e681a3d4eb63565d2862af82a3b
trbogi <trencsenyi.bogi@gmail.com>
2022-11-03 16:03:44 +0100

    Sign up

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,7 +1,38 @@
 import { Component } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {AuthService} from "./auth.service";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = false
+
+  constructor(private authService: AuthService) {
+  }
+
+  onSwitchMode(){
+    this.isLoginMode = !this.isLoginMode
+    console.log(this.isLoginMode)
+  }
+
+  onSubmit(form: NgForm){
+    if (!form.valid){
+      return
+    }
+
+    if (this.isLoginMode){
+
+    }else{
+      const email = form.value.email
+      const password = form.value.password
+      this.authService.signUp(email, password).subscribe(
+        resData => console.log(resData),
+        error => console.log(error)
+      )
+    }
+
+    form.reset()
+  }
+}
