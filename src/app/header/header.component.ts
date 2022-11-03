4975c77b0bb80ed828ff493e52992aab839eb824
trbogi <trencsenyi.bogi@gmail.com>
2022-11-03 12:29:20 +0100

    Store recipes on backend side

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,17 @@
 import {Component, EventEmitter, Output} from "@angular/core"
+import {DataStorageService} from "../shared/data-storage.service";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent{
   collapsed = true
+
+  constructor(private dataService: DataStorageService) {
+  }
+
+
+  onSaveData(){
+    this.dataService.storeRecipes()
+  }
 }
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {RecipeService} from "../recipes/recipe.service";
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {
+  }
+
+  storeRecipes(){
+    const recipes = this.recipeService.get()
+    this.http
+      .put('https://ng-course-recipe-book-383df-default-rtdb.europe-west1.firebasedatabase.app/recipes.json', recipes)
+      .subscribe(recipes => console.log(recipes))
+  }
+}
