8d54e619e6588a279b8cc446d3b993e671417425
trbogi <trencsenyi.bogi@gmail.com>
2022-11-04 11:05:36 +0100

    Login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForm} from "@angular/forms";
-import {AuthService} from "./auth.service";
+import {AuthResponseData, AuthService} from "./auth.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-auth',
@@ -24,23 +25,27 @@ export class AuthComponent {
       return
     }
 
+    let authResponse: Observable<AuthResponseData>
+    const email = form.value.email
+    const password = form.value.password
     this.isLoading = true
-    if (this.isLoginMode){
 
+    if (this.isLoginMode){
+      authResponse = this.authService.login(email, password)
     }else{
-      const email = form.value.email
-      const password = form.value.password
-      this.authService.signUp(email, password).subscribe(
-        resData => {
-          console.log(resData)
-          this.isLoading = false
-        },
-        errorMessage => {
-          this.error = errorMessage
-          this.isLoading = false
-        }
-      )
+      authResponse = this.authService.signUp(email, password)
     }
+
+    authResponse.subscribe(
+      resData => {
+        console.log(resData)
+        this.isLoading = false
+      },
+      errorMessage => {
+        this.error = errorMessage
+        this.isLoading = false
+      }
+    )
 
     form.reset()
   }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,12 +2,13 @@ import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {catchError, throwError} from "rxjs";
 
-interface AuthResponseData{
+export interface AuthResponseData{
   idToken: string,
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -33,5 +34,15 @@ export class AuthService{
         }
         return throwError(errorMessage)
     }))
+  }
+
+  login(email: string, password: string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCu1FLERZGeWfqE1bJVziwiJRcUcw1CKcA',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+      )
   }
 }
