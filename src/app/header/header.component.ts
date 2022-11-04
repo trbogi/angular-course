55490f41ab071d34ac33dfc992bbd099b6df553b
trbogi <trencsenyi.bogi@gmail.com>
2022-11-04 11:58:41 +0100

    Header reflects to authentication state

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,21 @@
-import {Component, EventEmitter, Output} from "@angular/core"
+import {Component, EventEmitter, OnDestroy, OnInit, Output} from "@angular/core"
 import {DataStorageService} from "../shared/data-storage.service";
+import {AuthService} from "../auth/auth.service";
+import {Subscription} from "rxjs";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent implements OnInit, OnDestroy{
   collapsed = true
+  isAuthenticated = false
+  authSub: Subscription
 
-  constructor(private dataService: DataStorageService) {
+  constructor(private dataService: DataStorageService, private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authSub = this.authService.user.subscribe(user => this.isAuthenticated = !!user)
   }
 
   onSaveData(){
@@ -17,4 +25,9 @@ export class HeaderComponent{
   onFetchData(){
     this.dataService.fetchRecipes().subscribe()
   }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe()
+  }
+
 }
